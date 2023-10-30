a21400e575fd85c2db0b19b512dff6d7404791e7
rabra <rabira.hierpa@gmail.com>
2023-10-30 11:53:00 +0300

    feat: adds rehype-raw for MD-HTML support - adds sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import tailwindCSSLogo from "./assets/tailwindcss.svg";
+import Markdown from "react-markdown";
+import gfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [markdown, setMarkdown] = useState<string>();
+
+  useEffect(() => {
+    fetch("/readme.md")
+      .then((res) => res.text())
+      .then((text) => {
+        setMarkdown(text);
+      });
+  }, []);
 
   return (
     <div className="App">
+      <Markdown
+        className={"flex flex-col space-x-1"}
+        remarkPlugins={[gfm]}
+        rehypePlugins={[remarkRehype, rehypeRaw]}
+        children={markdown}
+      />
       <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
